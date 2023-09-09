26a0e09534d82e108289a96cd85e2d8fa759752a
RiccFsl <115220962+RiccFsl@users.noreply.github.com>
2023-09-09 14:32:36 +0000

    l'indentazione ziocan

--- shampo_sales1.py
+++ shampo_sales1.py
@@ -2,8 +2,8 @@ my_file = open('shampoo_sales.txt', 'r')
 def sum_csv(file_name):
     values = []
     for line in file_name:
-    element = line.split(',')
-    if (element[0] != 'Date'):
-        value = element[1]
-        values.append(value)
+        element = line.split(',')
+        if (element[0] != 'Date'):
+            value = element[1]
+            values.append(value)
     return sum(values)

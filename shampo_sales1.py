903eb84bf9dd3d848d90b95d3757badb49251554
RiccFsl <115220962+RiccFsl@users.noreply.github.com>
2023-09-09 15:19:05 +0000

    djoakncd

--- shampo_sales1.py
+++ shampo_sales1.py
@@ -3,7 +3,7 @@ def sum_csv(file_name):
     file = open('file_name','r')
     for line in file:
         element = line.split(',')
-        if (element[0] != 'Date'):
+        if element[0] != 'Date':
             value = element[1]
             values.append(float(value))
     file.close()

8ee3124081ecede85191f5fbf5a17dfff2f76ee0
RiccFsl <115220962+RiccFsl@users.noreply.github.com>
2023-09-13 16:27:39 +0000

    .

---
+++ oggeti1.py
@@ -0,0 +1,17 @@
+class CSVFile():
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def get_data (self):
+        lista = []
+        try:
+            my_file = open(self.name, 'r') 
+        except Exception as e:
+            print('Errore! il file "{}" non esiste'.format(self.name))
+        for line in my_file:
+            element = line.split(',')
+            #se non sto processando l'intestazione
+            if element [0] != 'Date':
+                lista.append(element)
+        return lista
